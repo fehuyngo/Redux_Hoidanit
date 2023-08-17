b592175f44bc9598773a728bb0fa2c16a5f1fe08
fehuyngo <fe.huyngo@gmail.com>
2023-08-17 14:14:24 +0700

    count user

--- react-hook-basic-Hoi-Dan-IT/src/components/Header.js
+++ react-hook-basic-Hoi-Dan-IT/src/components/Header.js
@@ -2,8 +2,10 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { listUsers } = useSelector(state => state.user);
   return (
     <>
       <Navbar bg="light" expand="lg" className="bg-body-tertiary">
@@ -12,20 +14,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+              <NavDropdown style={{ marginLeft: "50px" }} title={`(${listUsers.length}) Users`} id="basic-nav-dropdown">
+                {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
+                  return (
+                    <NavDropdown.Item key={`user-${index}`}>{item.email}</NavDropdown.Item>
+                  )
+                })}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
