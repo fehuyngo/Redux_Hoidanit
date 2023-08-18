ab971ccb7bd388033bb3d3eedb28c8bf566c93d9
fehuyngo <fe.huyngo@gmail.com>
2023-08-18 09:21:07 +0700

    fetch data no redux

--- react-hook-basic-Hoi-Dan-IT-redux-toolkit/src/App.js
+++ react-hook-basic-Hoi-Dan-IT-redux-toolkit/src/App.js
@@ -1,12 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    fetchAllUser();
+  },[]);
+
+  const fetchAllUser = async () => {
+    const res = await axios.get("http://localhost:8080/users/all");
+    setListUsers(res ? res.data : []);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        {/* <h1>Hello world with React and Hoi Dan IT!</h1>
+        <div className='btn-actions'>
+          <button onClick={() => dispatch(increment())}>Increase</button>
+          <button onClick={() => dispatch(decrement())}>Decrease</button>
+        </div>
+        <br />
+        <div>Count = {count}</div> */}
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Username</th>
+          </thead>
+          <tbody>
+            {listUsers && listUsers.length && listUsers.map((item, index) => {
+              return (
+                <tr key={`table-redux-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.username}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </header>
     </div>
   );
